bddcadbae944512fe157503ee3933575aef5b365
Martin Curran <martincurran28@hotmail.com>
2019-03-17 16:58:04 +0000

    Tied up the views and urls for cart functionality. Also created a simple cart.html. Usercan now login, chose a quantity of an item and add that item to their cart. Media image isn't appearing, but will worry about that later.

---
+++ cart/contexts.py
@@ -0,0 +1,18 @@
+from django.shortcuts import get_object_or_404
+from services.models import Service
+
+def cart_contents(request):
+    
+    cart = request.session.get('cart', {})
+
+    cart_items = []
+    total = 0
+    service_count = 0
+    
+    for id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=id)
+        total += quantity * service.price
+        service_count += quantity
+        cart_items.append({'id': id, 'quantity': quantity, 'service': service})
+    
+    return {'cart_items': cart_items, 'total': total, 'service_count': service_count}
