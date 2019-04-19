b31514996312c47a646a60dafb116c28e6774ddd
Martin Curran <martincurran28@hotmail.com>
2019-04-19 23:04:03 +0000

    Added new tests to the tests.py file

--- services/tests.py
+++ services/tests.py
@@ -11,5 +11,15 @@ class ServiceTest(TestCase):
         test_name = Service(name = 'A Service')
         self.assertEqual(str(test_name), 'A Service')
         
+    def test_str_desc(self):
+        test_desc = Service(name = 'This service is provided by Bedbug')
+        self.assertEqual(str(test_desc), 'This service is provided by Bedbug')
+        
+    def test_str_price(self):
+        test_price = Service(name = '20.00')
+        self.assertEqual(str(test_price), '20.00')    
+        
+    
+        
 
                 
