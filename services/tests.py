7f16086ed13026faeefd5c11ccfd1e9d26b1199b
Martin Curran <martincurran28@hotmail.com>
2019-04-24 20:17:07 +0000

    Further tests performed for test.py.

--- services/tests.py
+++ services/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Service
+from services.models import Service
 
 class ServiceTest(TestCase):
     """
@@ -10,9 +10,18 @@ class ServiceTest(TestCase):
     def test_str(self):
         test_name = Service(name = 'A Service')
         self.assertEqual(str(test_name), 'A Service')
+        
     def test_str_desc(self):
         test_desc = Service(name = 'This service is provided by Bedbug')
         self.assertEqual(str(test_desc), 'This service is provided by Bedbug')
+        
+    def test_str_likes(self):
+        test_likes = Service(name = str(1))
+        self.assertEqual(str(test_likes), str(1)) 
+        
+    def test_str_views(self):
+        test_views = Service(name = str(1))
+        self.assertEqual(str(test_views), str(1))    
         
     def test_str_price(self):
         test_price = Service(name = '20.00')
