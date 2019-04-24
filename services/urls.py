3832360cfc5eb99792cdc57c4ef71aa61ce6200d
Martin Curran <martincurran28@hotmail.com>
2019-04-24 14:54:15 +0000

    Added a like button to the Service Model.

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_services, homepage
+from .views import all_services, service_like, homepage
 
 urlpatterns = [
     url(r'^$', all_services, name = 'services'),
+    url(r'^/(?P<pk>[0-9]+)/like/$', service_like, name='service_like'),
     url(r'^home/$', homepage, name = 'homepage'),
     ]
--- services/views.py
+++ services/views.py
@@ -3,7 +3,19 @@ from .models import Service
 
 def homepage(request):
     services = Service.objects.all()
+    for service in services:
+        service.views += 1
+        service.save()
     return render(request, 'services.html', {"services": services})
+    
+def service_like(request, pk):
+    if pk:
+        service = Service.objects.get(id=pk)
+        count = service.likes
+        count += 1
+        service.likes = count
+        service.save()
+    return redirect('homepage')    
     
 def all_services(request):
     services = Service.objects.all()
