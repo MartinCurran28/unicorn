5d77c3fb7460eac8d1833b122798f524d559bc24
Martin Curran <martincurran28@hotmail.com>
2019-04-24 14:41:40 +0000

    Touched up some templates.

--- services/models.py
+++ services/models.py
@@ -5,6 +5,8 @@ class Service(models.Model):
     description = models.TextField()
     views = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     image = models.ImageField(upload_to='image')
     
     def __str__(self):
